8b76c688c644cd7c57f632fc563610ec2db86c20
Jack Wright <jack.wright@disqo.com>
2024-08-16 14:37:38 -0700

    Added aws config and moved parse_url to

--- src/cache.rs
+++ src/cache.rs
@@ -5,13 +5,10 @@ use std::{
 
 use bytes::Bytes;
 use nu_protocol::{ShellError, Span, Spanned};
-use object_store::{
-    parse_url_opts, path::Path, Error as ObjectStoreError, GetOptions, ObjectStore,
-    ObjectStoreScheme,
-};
+use object_store::{path::Path, Error as ObjectStoreError, GetOptions, ObjectStore};
 use url::Url;
 
-use crate::aws;
+use crate::parse_url;
 
 pub struct CacheEntry {
     path: Path,
@@ -82,29 +79,4 @@ fn cache_get_error(e: impl std::error::Error) -> ShellError {
         help: None,
         inner: vec![],
     }
-}
-
-pub async fn parse_url(
-    url: &Spanned<Url>,
-    span: Span,
-) -> Result<(Box<dyn ObjectStore>, Path), ShellError> {
-    let (scheme, _) =
-        ObjectStoreScheme::parse(&url.item).map_err(|e| ShellError::IncorrectValue {
-            msg: format!("Unsupported url: {e}"),
-            val_span: url.span,
-            call_span: span,
-        })?;
-
-    let options: Vec<(String, String)> = match scheme {
-        ObjectStoreScheme::AmazonS3 => aws::options().await?,
-        _ => {
-            vec![]
-        }
-    };
-
-    parse_url_opts(&url.item, options).map_err(|e| ShellError::IncorrectValue {
-        msg: format!("Unsupported url: {e}"),
-        val_span: url.span,
-        call_span: span,
-    })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use bytes::Bytes;
 use cache::Cache;
 use nu_plugin::Plugin;
 use nu_protocol::{ShellError, Span, Spanned};
+use object_store::{parse_url_opts, path::Path, ObjectStore, ObjectStoreScheme};
 use tokio::runtime::Runtime;
 use url::Url;
 
@@ -53,4 +54,29 @@ impl Plugin for CloudPlugin {
     fn commands(&self) -> Vec<Box<dyn nu_plugin::PluginCommand<Plugin = Self>>> {
         vec![Box::new(stub::Stub), Box::new(open::Open)]
     }
+}
+
+pub async fn parse_url(
+    url: &Spanned<Url>,
+    span: Span,
+) -> Result<(Box<dyn ObjectStore>, Path), ShellError> {
+    let (scheme, _) =
+        ObjectStoreScheme::parse(&url.item).map_err(|e| ShellError::IncorrectValue {
+            msg: format!("Unsupported url: {e}"),
+            val_span: url.span,
+            call_span: span,
+        })?;
+
+    let options: Vec<(String, String)> = match scheme {
+        ObjectStoreScheme::AmazonS3 => aws::options().await?,
+        _ => {
+            vec![]
+        }
+    };
+
+    parse_url_opts(&url.item, options).map_err(|e| ShellError::IncorrectValue {
+        msg: format!("Unsupported url: {e}"),
+        val_span: url.span,
+        call_span: span,
+    })
 }
