ca427db031eabf843e048cbdfa2f38c51d8e0d0b
Jack Wright <jack.wright@disqo.com>
2024-08-22 15:51:22 -0700

    changing the way url parsing works

--- src/providers/aws.rs
+++ src/providers/aws.rs
@@ -2,36 +2,74 @@ use std::error::Error;
 
 use aws_config::{BehaviorVersion, SdkConfig};
 use aws_credential_types::{provider::ProvideCredentials, Credentials};
-use nu_protocol::ShellError;
-use object_store::aws::AmazonS3ConfigKey;
+use nu_protocol::{ShellError, Spanned};
+use object_store::aws::{AmazonS3Builder, AmazonS3ConfigKey};
+use url::Url;
 
-pub async fn options() -> Result<Vec<(String, String)>, ShellError> {
+use super::NuObjectStore;
+
+pub async fn parse_url(url: &Spanned<Url>) -> Result<NuObjectStore, ShellError> {
     let aws_config = aws_load_config().await;
-    let mut options: Vec<(String, String)> = Vec::with_capacity(3);
-    if let Some(region) = aws_config.region() {
-        options.push((
-            AmazonS3ConfigKey::Region.as_ref().to_string(),
-            region.to_string(),
-        ));
-    }
-    if let Some(credentials) = aws_creds(&aws_config).await? {
-        options.push((
-            AmazonS3ConfigKey::AccessKeyId.as_ref().to_string(),
-            credentials.access_key_id().to_string(),
-        ));
-        options.push((
-            AmazonS3ConfigKey::SecretAccessKey.as_ref().to_string(),
-            credentials.secret_access_key().to_string(),
-        ));
-        if let Some(token) = credentials.session_token() {
-            options.push((
-                AmazonS3ConfigKey::Token.as_ref().to_string(),
-                token.to_string(),
-            ))
-        }
-    }
+    let builder = AmazonS3Builder::new().with_url(url.item.clone());
 
-    Ok(options)
+    let bucket_name = builder
+        .get_config_value(&AmazonS3ConfigKey::Bucket)
+        .ok_or_else(|| ShellError::GenericError {
+            error: format!(
+                "Could not determine Amazon S3 bucket name from url {}",
+                url.item
+            ),
+            msg: "".into(),
+            span: Some(url.span),
+            help: None,
+            inner: vec![],
+        })?;
+
+    let (builder, region) = if let Some(region) = aws_config.region() {
+        (builder.with_region(region.to_string()), region.to_string())
+    } else {
+        return Err(ShellError::GenericError {
+            error: "Could not determine AWS region from environment".into(),
+            msg: "".into(),
+            span: Some(url.span),
+            help: None,
+            inner: vec![],
+        });
+    };
+
+    let builder = if let Some(credentials) = aws_creds(&aws_config).await? {
+        let builder = builder
+            .with_access_key_id(credentials.access_key_id())
+            .with_secret_access_key(credentials.secret_access_key());
+
+        if let Some(token) = credentials.session_token() {
+            builder.with_token(token)
+        } else {
+            builder
+        }
+    } else {
+        return Err(ShellError::GenericError {
+            error: "Could not determine AWS credentials from environment".into(),
+            msg: "".into(),
+            span: Some(url.span),
+            help: None,
+            inner: vec![],
+        });
+    };
+
+    let s3 = builder.build().map_err(|e| ShellError::GenericError {
+        error: format!("Could not create Amazon S3 client: {e}"),
+        msg: "".into(),
+        span: Some(url.span),
+        help: None,
+        inner: vec![],
+    })?;
+
+    Ok(NuObjectStore::AmazonS3 {
+        store: Box::new(s3),
+        bucket: bucket_name,
+        region,
+    })
 }
 
 async fn aws_load_config() -> SdkConfig {
--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,14 +1,19 @@
 mod aws;
 
 use nu_protocol::{ShellError, Span, Spanned};
-use object_store::parse_url_opts;
+use object_store::local::LocalFileSystem;
+use object_store::memory::InMemory;
 use object_store::{path::Path, ObjectStore, ObjectStoreScheme};
 use url::Url;
 
 pub enum NuObjectStore {
     Local(Box<dyn ObjectStore>),
     Memory(Box<dyn ObjectStore>),
-    AmazonS3(Box<dyn ObjectStore>),
+    AmazonS3 {
+        store: Box<dyn ObjectStore>,
+        bucket: String,
+        region: String,
+    },
     #[allow(dead_code)]
     GoogleCloudStorage(Box<dyn ObjectStore>),
     #[allow(dead_code)]
@@ -17,20 +22,11 @@ pub enum NuObjectStore {
     Http(Box<dyn ObjectStore>),
 }
 impl NuObjectStore {
-    pub fn new(scheme: ObjectStoreScheme, object_store: Box<dyn ObjectStore>) -> Self {
-        match scheme {
-            ObjectStoreScheme::Local => NuObjectStore::Local(object_store),
-            ObjectStoreScheme::Memory => NuObjectStore::Memory(object_store),
-            ObjectStoreScheme::AmazonS3 => NuObjectStore::AmazonS3(object_store),
-            _ => unimplemented!(),
-        }
-    }
-
     pub fn object_store(&self) -> &dyn ObjectStore {
         match self {
             NuObjectStore::Local(store) => store.as_ref(),
             NuObjectStore::Memory(store) => store.as_ref(),
-            NuObjectStore::AmazonS3(store) => store.as_ref(),
+            NuObjectStore::AmazonS3 { store, .. } => store.as_ref(),
             NuObjectStore::GoogleCloudStorage(store) => store.as_ref(),
             NuObjectStore::MicrosoftAzure(store) => store.as_ref(),
             NuObjectStore::Http(store) => store.as_ref(),
@@ -42,26 +38,37 @@ pub async fn parse_url(
     url: &Spanned<Url>,
     span: Span,
 ) -> Result<(NuObjectStore, Path), ShellError> {
-    let (scheme, _) =
+    let (scheme, path) =
         ObjectStoreScheme::parse(&url.item).map_err(|e| ShellError::IncorrectValue {
             msg: format!("Unsupported url: {e}"),
             val_span: url.span,
             call_span: span,
         })?;
 
-    let options: Vec<(String, String)> = match scheme {
-        ObjectStoreScheme::AmazonS3 => aws::options().await?,
+    let path = Path::parse(path).map_err(|e| ShellError::IncorrectValue {
+        msg: format!("Unsupported path: {e}"),
+        val_span: url.span,
+        call_span: span,
+    })?;
+
+    let object_store = match scheme {
+        ObjectStoreScheme::AmazonS3 => aws::parse_url(url).await?,
+        ObjectStoreScheme::Local => {
+            let store = LocalFileSystem::new();
+            NuObjectStore::Local(Box::new(store))
+        }
+        ObjectStoreScheme::Memory => {
+            let store = InMemory::new();
+            NuObjectStore::Memory(Box::new(store))
+        }
         _ => {
-            vec![]
+            return Err(ShellError::IncorrectValue {
+                msg: format!("Unsupported url: {}", url.item),
+                val_span: url.span,
+                call_span: span,
+            })
         }
     };
 
-    let (object_store, path) =
-        parse_url_opts(&url.item, options).map_err(|e| ShellError::IncorrectValue {
-            msg: format!("Unsupported url: {e}"),
-            val_span: url.span,
-            call_span: span,
-        })?;
-
-    Ok((NuObjectStore::new(scheme, object_store), path))
+    Ok((object_store, path))
 }
