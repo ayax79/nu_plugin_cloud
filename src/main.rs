d163af7727067460bb660059fc5f959098230e02
Jack Wright <jack.wright@disqo.com>
2024-08-23 16:29:53 -0700

    Using mimalloc for the global allocator

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use log::debug;
 use nu_plugin::{serve_plugin, MsgPackSerializer};
 use nu_plugin_cloud::CloudPlugin;
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 fn main() {
     env_logger::init();
     debug!("Starting cloud plugin");
