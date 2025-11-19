526988a5cfea8ba82fd83f4826c3a837e75639b6
Jack Wright <jack.wright@nike.com>
2025-11-19 12:08:46 -0800

    starting to borrow stuff from the nushell flake

--- default.nix
+++ default.nix
@@ -1,32 +1,117 @@
-{ pkgs ? import <nixpkgs> { }, stdenv ? pkgs.stdenv, lib ? stdenv.lib
-  # A set providing `buildRustPackage :: attrsets -> derivation`
-, rustPlatform ? pkgs.rustPlatform, fetchFromGitHub ? pkgs.fetchFromGitHub
-, gitignoreSrc ? null, pkgconfig ? pkgs.pkgconfig, gtk3 ? pkgs.gtk3
-, glib ? pkgs.glib, gobject-introspection ? pkgs.gobject-introspection }:
+# Generic builder for nushell and its included plugins.
+{
+  stdenv,
+  lib,
+  rustPlatform,
+  # Allows overriding for the plugins
+  pname ? "nushell",
+  # Use this for the cargo file that the plugin has
+  extraCargo ? ../../Cargo.toml,
+  withDefaultFeatures ? true,
+  additionalFeatures ? (p: p),
+  # Package dependencies
+  pkg-config,
+  openssl,
+  curlMinimal,
+  python3,
+  xorg,
+  nghttp2,
+  libgit2,
+  zstd,
+  zlib,
+  # Mostly args and things that might be different for plugins
+  nativeBuildInputs ? [
+    pkg-config
+  ]
+  ++ lib.optionals (withDefaultFeatures && stdenv.hostPlatform.isLinux) [ python3 ]
+  ++ lib.optionals stdenv.cc.isClang [ rustPlatform.bindgenHook ],
+  buildInputs ? [
+    zstd
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [ zlib ]
+  ++ lib.optionals (withDefaultFeatures && stdenv.hostPlatform.isLinux) [ xorg.libX11 ]
+  ++ lib.optionals (withDefaultFeatures && stdenv.hostPlatform.isDarwin) [
+    nghttp2
+    libgit2
+  ],
+  checkInputs ?
+    lib.optionals stdenv.hostPlatform.isDarwin [ curlMinimal ]
+    ++ lib.optionals stdenv.hostPlatform.isLinux [ openssl ],
 
+  # Fixed in #16914
+  buildAndTestSubdir ? ".",
+  checkFlags ? [ ],
+  buildType ? "release",
+  checkType ? buildType,
+  ...
+}@args:
 let
-  gitignoreSource = if gitignoreSrc != null then
-    gitignoreSrc.gitignoreSource
-  else
-    (import (fetchFromGitHub {
-      owner = "hercules-ci";
-      repo = "gitignore";
-      rev = "c4662e662462e7bf3c2a968483478a665d00e717";
-      sha256 = "0jx2x49p438ap6psy8513mc1nnpinmhm8ps0a4ngfms9jmvwrlbi";
-    }) { inherit lib; }).gitignoreSource;
-in rustPlatform.buildRustPackage rec {
-  pname = "nu_plugin_cloud";
-  version = "0.2.10+0.107.0";
+  inherit (builtins)
+    fromTOML
+    readFile
+    head
+    ;
+  inherit (lib.fileset)
+    toSource
+    intersection
+    unions
+    difference
+    ;
+  root = ../..;
+  cargoToml = fromTOML (readFile (root + /Cargo.toml));
+  # Allow for extra Cargo.toml files to be passed for crates
+  extraToml = fromTOML (readFile extraCargo);
+in
+rustPlatform.buildRustPackage {
+  inherit
+    pname
+    nativeBuildInputs
+    buildInputs
+    checkInputs
+    buildAndTestSubdir
+    checkFlags
+    buildType
+    checkType
+    ;
+  inherit (cargoToml.package) version;
 
-  src = gitignoreSource ./.;
+  # Crates will need to build with their Cargo.toml
+  src = toSource {
+    inherit root;
+    fileset = (
+      intersection root (
+        difference (unions (
+          map (p: root + p) [
+            /Cargo.toml
+            /Cargo.lock
+            /src
+          ]
+        )) ./.
+      )
+    );
+  };
 
-  buildInputs = [ gtk3 glib gobject-introspection ];
-  nativeBuildInputs = [ pkgconfig ];
-  cargoSha256 = "sha256-0hfmV4mbr3l86m0X7EMYTOu/b+BjueVEbbyQz0KgOFY=";
+  cargoLock = {
+    lockFile = root + /Cargo.lock;
+    # Required for some of the dependencies in this repo
+    allowBuiltinFetchGit = true;
+  };
 
-  meta = with stdenv.lib; {
-    homepage = "";
-    description = "Sample flake repository for a Rust application";
-    license = licenses.mit;
+  buildNoDefaultFeatures = !withDefaultFeatures;
+  buildFeatures = additionalFeatures [ ];
+
+  # Builds the plugins for the test using the right profile, otherwise the
+  # plugins cannot be found.
+  NUSHELL_CARGO_PROFILE = checkType;
+  NU_TEST_LOCALE_OVERRIDE = "en_US.UTF-8";
+  preCheck = ''
+    export HOME=$(mktemp -d)
+  '';
+  meta = {
+    description = extraToml.package.description or cargoToml.package.description;
+    homepage =
+      if cargoToml == extraToml then cargoToml.package.homepage else extraToml.package.repository or null;
+    license = lib.licenses.mit;
+    mainProgram = (head (extraToml.bin or [ extraToml.package ])).name or pname;
   };
 }
--- flake.nix
+++ flake.nix
@@ -1,26 +1,127 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    systems.url = "github:nix-systems/default";
-    rust-flake.url = "github:juspay/rust-flake";
-    rust-flake.inputs.nixpkgs.follows = "nixpkgs";
-    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
-    cargo-doc-live.url = "github:srid/cargo-doc-live";
-
-    git-hooks.url = "github:cachix/git-hooks.nix";
-    git-hooks.flake = false;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-
-      # See ./nix/modules/*.nix for the modules that are imported here.
-      imports = with builtins;
-        map
-          (fn: ./nix/modules/${fn})
-          (attrNames (readDir ./nix/modules));
+  outputs =
+    inputs:
+    let
+      inherit (inputs.nixpkgs) lib;
+      # systems = lib.systems.flakeExposed;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "powerpc64le-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "riscv64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+        "i686-linux"
+        ## Broken in `nix flake check`
+        # "x86_64-freebsd"
+      ];
+      main-overlay = final: previous: {
+        # Override the rustPlatform without forcing a rebuild of _every_ tool as well
+        rustToolchain' = previous.rust-bin.fromRustupToolchainFile ../../rust-toolchain.toml;
+        rustPlatform' = final.makeRustPlatform {
+          cargo = final.rustToolchain';
+          rustc = final.rustToolchain';
+        };
+        # Add a specific rustPlatform with extensions for development/editors
+        rustToolchain-devshell = final.rustToolchain'.override ({
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+          ];
+        });
+        rustPlatform-devshell = final.makeRustPlatform {
+          cargo = final.rustToolchain-devshell;
+          rustc = final.rustToolchain-devshell;
+        };
+        nu_plugin_cloud = previous.callPackage ./. {
+          rustPlatform = final.rustPlatform';
+        };
+      };
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs systems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.rust-overlay.overlays.default
+                main-overlay
+              ];
+            };
+          }
+        );
+    in
+    {
+      overlays = {
+        default = main-overlay;
+      };
+      packages = forEachSupportedSystem (
+        { pkgs, ... }:
+        {
+          inherit (pkgs) nushell;
+          default = pkgs.nushell;
+          dev = pkgs.nushell.override {
+            buildType = "dev";
+          };
+        }
+      );
+      legacyPackages = forEachSupportedSystem (
+        { pkgs, ... }:
+        {
+          nushellPlugins = {
+            # Only getting a subset of these so we don't have _all_ of the
+            # plugins that are in `nixpkgs`
+            inherit (pkgs.nushellPlugins)
+              cloud
+              formats
+              gstat
+              inc
+              polars
+              query
+              ;
+          };
+        }
+      );
+      devShells = forEachSupportedSystem (
+        { pkgs, ... }:
+        {
+          # devshell with build/check deps from nushell and plugins, with some
+          # separation for linux/darwin.
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              # (
+                [
+                  rustToolchain-devshell
+                  rustPlatform-devshell.bindgenHook
+                  # curlMinimal
+                  # openssl
+                  # pkg-config
+                  # zstd
+                ];
+                # ++ (lib.optionals stdenv.hostPlatform.isLinux [
+                #   python3
+                #   xorg.libX11
+                # ])
+                # ++ (lib.optionals stdenv.hostPlatform.isDarwin [
+                #   zlib
+                #   nghttp2
+                #   libgit2
+                # ])
+              # );
+          };
+        }
+      );
     };
 }
